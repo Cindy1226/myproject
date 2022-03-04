98dac466904204f43458493cec5036402ea552fb
cindy1226 <cindywawa91@gmail.com>
2022-03-04 16:11:27 +0800

    student score

---
+++ src/main/java/com/cindy/Scoring.java
@@ -0,0 +1,26 @@
+package com.cindy;
+
+public class Scoring {
+    public static void main(String[] args) {
+        student tom = new student();
+        tom.name="Tom";
+        tom.Eng=80;
+        tom.Math=60;
+        student amy = new student();
+        amy.name="Amy";
+        amy.Eng=90;
+        amy.Math=85;
+        student bob = new student();
+        bob.name="Bob";
+        bob.Eng=85;
+        bob.Math=74;
+        student ray = new student();
+        ray.name="Ray";
+        ray.Eng=70;
+        ray.Math=80;
+        student ivy = new student();
+        ivy.name="Ivy";
+        ivy.Eng=75;
+        ivy.Math=79;
+    }
+}
---
+++ src/main/java/com/cindy/student.java
@@ -0,0 +1,7 @@
+package com.cindy;
+
+public class student {
+    String name;
+    int Eng;
+    int Math;
+}
