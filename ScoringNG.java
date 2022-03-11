72302c5b0e580564d7ed5eaae99cc1ebd5793440
Cindy1226 <93966287+Cindy1226@users.noreply.github.com>
2022-03-11 16:30:09 +0800

    Add files via upload

---
+++ ScoringNG.java
@@ -0,0 +1,16 @@
+package com.cindy;
+
+public class ScoringNG {
+    public static void main(String[] args) {
+        String[] name ={"Tom","Amy","Bob","Ray","Ivy"};
+        int[] Eng={80,90,85,70,75};
+        int[] Math={60,85,74,80,79};
+        for (int i = 0; i < 5; i++) {
+            if (i!=1){
+                System.out.println(name[i]+"\t"+Eng[i]+"\t"+Math[i]+"\t"+(Eng[i]+Math[i])/2);
+            }
+
+        }
+
+    }
+}
