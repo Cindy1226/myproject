397e13a96ba2e1d8b395554ef9f8dc53cd03c822
User <cindywawa91@gmail.com>
2022-03-18 14:33:15 +0800

    commit

--- src/main/java/com/cindy/Scoring.java
+++ src/main/java/com/cindy/Scoring.java
@@ -2,25 +2,34 @@ package com.cindy;
 
 public class Scoring {
     public static void main(String[] args) {
-        student tom = new student();
-        tom.name="Tom";
-        tom.Eng=80;
-        tom.Math=60;
-        student amy = new student();
-        amy.name="Amy";
-        amy.Eng=90;
-        amy.Math=85;
-        student bob = new student();
-        bob.name="Bob";
+        student tom = new student("tom",80,60);
+        // tom.name="Tom";
+        // tom.Eng=80;
+        // tom.Math=60;
+        student amy=new student("amy",90,85);
+        tom.print();
+        amy.print();
+
+        student s = new student();
+        s.print();
+        student bob = new student("bob",85,74);
+        //bob.name="Bob";
         bob.Eng=85;
         bob.Math=74;
-        student ray = new student();
-        ray.name="Ray";
+        bob.print();
+        student ray = new student("ray",70,80);
+        // ray.name="Ray";
         ray.Eng=70;
         ray.Math=80;
-        student ivy = new student();
-        ivy.name="Ivy";
+        ray.print();
+        student ivy = new student("ivy",75,79);
+        // ivy.name="Ivy";
         ivy.Eng=75;
         ivy.Math=79;
+        ivy.print();
+        GraduateStudent jay=new GraduateStudent("jay",65,82,79);
+        jay.name="jay";
+        jay.print();
     }
 }
+
